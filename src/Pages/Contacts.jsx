7d37baee88974b292c945165b28aef4f68671b90
tigrastov <tigrastov@yandex.ru>
2025-06-08 19:51:08 +0300

    contacts

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,11 +1,55 @@
 import './Contacts.css';
 
+
 export default function Contacts() {
+
+
+
   return (
     <div className='contacts'>
       <div className='contacts-container'>
-        <h1>Contacts</h1>
+
+
+        <div className='text-content'>
+
+          <h2>Контакты</h2>
+
+        <div className='link-container'>
+           <img src='/phone.png' className='icon-link' alt='phone'/>
+        <a href="tel:+79123456789" className="contact-link">+7 (912) 345-67-89</a>
+        </div>
+
+
+        <div className='link-container'>
+          <img src='/telega.png'className='icon-link' alt='telega'/>
+        <a href="https://t.me/username" target="_blank" className="contact-link">Telegra</a>
+        </div>
+
+
+         <div className='link-container'>
+           <img src='mail.png'className='icon-link'alt='mail'/>
+         <a href="mailto:example@mail.com" className="contact-link">
+            <span>example@mail.com</span>
+          </a>
+        </div>
+
+        </div>
+        
+        <div className='callback-form'>
+          <img src='/Digital.png' alt='Portrait' className='profile-photo'/>
+
+        </div>
+
+
+
+
+
+
+
+
+
       </div>
+
     </div>
 
 
